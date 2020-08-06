2c5d8a472442229d41753320dc20262d4392f151
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2020-08-06 17:04:30 +0200

    15_06_08_2020 (Nizovi vežbanje)

--- 14_04_08_2020/nizovi/script.js
+++ 14_04_08_2020/nizovi/script.js
@@ -26,7 +26,7 @@ for(let i = 0; i <= cars.length - 1; i++) {
 }
 
 // Odrediti zbir elemenata celobrojnog niza.
-let niz = [9, 15, 3, -4, -10, 2, 1];
+let niz = [9, 15, 3, -4, -10, 20, 1];
 /*let sum = 0;
 for(let i = 0; i < niz.length; i++) {
     sum += niz[i];
@@ -42,3 +42,147 @@ let suma = myarray => {
 }
 
 console.log(suma(niz));
+
+//4. Srednja vrednost (aritmetička sredina) elemenata niza
+let avg = myarr => {
+    let s = suma(myarr);
+    let arsr = s / myarr.length;
+    return arsr;
+}
+console.log("Aritmetička sredina je: " + avg(niz));
+
+//5. Maksimalni element u nizu
+let max = myarr => {
+    let maks = myarr[0];
+    for(let i=0; i<myarr.length; i++){
+        if(maks < myarr[i]){
+            maks = myarr[i];
+        }
+    }
+    return maks;
+}
+console.log("Najveći element niza je: " + max(niz));
+
+//6. Minimalni element u nizu
+let min = myarr => {
+    let mini = myarr[0];
+    for(let i=0; i<myarr.length; i++){
+        if(mini > myarr[i]){
+            mini = myarr[i];
+        }
+    }
+    return mini;
+}
+console.log("Najmanji element niza je: " + min(niz));
+
+//7. Odrediti indeks maksimalnog elementa celobrojnog niza.
+//1. način
+let maxInd = myarr => {
+    let maks = myarr[0];
+    let maksInd = 0;
+
+    for(let i=0; i<myarr.length; i++){
+        if(myarr[i] > maks){
+            maks = myarr[i];
+            maksInd = i;
+        }
+    }
+
+    return maksInd;
+}
+console.log("Indeks najvećeg elementa je " + maxInd(niz));
+
+//2. način
+let maxInd2 = myarr => {
+    let maks = max(myarr);
+    for(let i=0; i<myarr.length; i++){
+        if(myarr[i] == maks){
+            return i;
+        }
+    }
+}
+console.log("Indeks najvećeg elementa je " + maxInd2(niz));
+
+// 9. Odrediti broj elemenata celobrojnog niza koji su veći od srednje vrednosti.
+//1. način - "pešaka"
+let br = 0;
+let sumaNiza = 0;
+for(let i=0; i<niz.length; i++){
+    sumaNiza += niz[i];
+}
+let avgNiza = sumaNiza/niz.length; 
+
+/*
+5 4 3 4 5
+srednja = 4
+br = 2
+*/
+for(let i=0; i<niz.length; i++){
+    if(niz[i] > avgNiza){
+        br++;
+    }
+}
+console.log("9. zadatak " + br);
+
+//2. način
+let brElemVecihOdArSr = arr => {
+    let arsr = avg(arr);
+    let br = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > arsr){
+            br++;
+        }
+    }
+    return br;
+};
+console.log("Broj elemenata većih od aritmetičke sredine: " + brElemVecihOdArSr(niz));
+
+// Suma elemenata većih od aritmetičke
+let sumaElemVecihOdArSr = arr => {
+    let arsr = avg(arr);
+    let s = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > arsr){
+            s += arr[i];
+        }
+    }
+    return s;
+};
+console.log("Suma elemenata većih od aritmetičke sredine: " + sumaElemVecihOdArSr(niz));
+
+// 12. zadatak - Elment paran ali indeks neparan
+/*
+if(arr[i]%2==0 && i%2!=0)
+*/
+
+// 14. zadatak - Promeniti znak svakom elementu celobrojnog niza.
+let a = 5;
+a = a * (-1); //a *= -1; //a = -a;
+
+let promeniZnak = myarr => {
+    for(let i=0; i<myarr.length; i++){
+        myarr[i] = myarr[i] * (-1);
+    }
+    console.log(myarr);
+};
+promeniZnak(niz);
+console.log(niz);
+
+/*
+15. zadatak
+if(myarr[i]%2 != 0 && i%2==0 )
+*/
+
+/*
+16. zadatak
+Dat je niz stavki za kupovinu (članovi niza su stringovi). Prolaskom kroz niz napraviti neuređenu listu i ispisati je u html dokument.
+*/
+let nizStr = ["Breskve", "Šljive", "Kajsije", "Jagode", "Maline"];
+let myList = document.getElementById('lista');
+for(let i=0; i<nizStr.length; i++){
+    myList.innerHTML += `<li> ${nizStr[i]} </li>`;
+}
+
+for(let i=nizStr.length-1; i>=0; i--){
+    myList.innerHTML += `<li> ${nizStr[i]} </li>`;
+}
