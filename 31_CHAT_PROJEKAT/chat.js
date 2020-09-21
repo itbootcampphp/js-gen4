06fdd52c8a99e1ba68308cd3d5e741d512334f7e
itbootcampphp <itbootcampphp@gmail.com>
2020-09-21 15:10:27 +0200

    31_CHAT_PROJEKAT (3. async dodavanje poruke)

--- 31_CHAT_PROJEKAT/chat.js
+++ 31_CHAT_PROJEKAT/chat.js
@@ -22,13 +22,39 @@ class Chatroom{
     get room(){
         return this._room;
     }
+
+    //Dodavanje nove poruke
+    async addChat(mess){
+
+        //Dohvatanje tekućeg datuma koji je potreban za timestamp
+        let date = new Date();
+
+        // Kreiranje objekta/dokumenta koji prosleđujemo bazi podataka
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(date)
+        };
+
+        //Da sačuvamo dokument u bazi
+        let response = await this.chats.add(docChat); 
+        return response;
+    }
 }
 //Nova instanca klase (Novi objekat)
 let chatroom1 = new Chatroom('js', 'SS');
 console.log(chatroom1.username, chatroom1.room);
+//chatroom1.addChat('Zravo svima!')
 
 let chatroom2 = new Chatroom('general', 'MĐ');
 console.log(chatroom2.username, chatroom2.room);
+// chatroom2.addChat('HR trening!')
+//     .then( () => {console.log('Uspešno dodat čet!');})
+//     .catch( err => {console.log(err);});
 
 let chatroom3 = new Chatroom('tests', 'JM');
 console.log(chatroom3.username, chatroom3.room);
+
+let chatroom4 = new Chatroom('tests', 'SS');
+console.log(chatroom4.username, chatroom4.room);
